c4ffeb7d553395bcce40c756831a9c43f59697b3
Daniel Correa <yo@danielgara.com>
2021-04-29 14:16:12 -0500

    imp: chapter04 implemented

---
+++ chapter03/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <Puzzles @puzzle-changed="selectedPuzzleId = $event" />
+    <Records />
+    <SliderPuzzle :puzzleId="selectedPuzzleId" />
+  </div>
+</template>
+
+<script>
+import SliderPuzzle from './components/SliderPuzzle.vue';
+import Puzzles from './components/Puzzles.vue';
+import Records from './components/Records.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SliderPuzzle,
+    Puzzles,
+    Records,
+  },
+  data() {
+    return {
+      selectedPuzzleId: 'cut-pink',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
